97b985fb2ce3ecbcdfa7ea7ee0598102a97e553e
aiputri <aliffia375@gmail.com>
2025-06-16 14:41:00 +0700

    UI lagi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
             <img
               className="object-cover object-center rounded"
               alt="hero"
-              src="https://stock.adobe.com/id/search?k=shopping&asset_id=241431868"
+              src="../image/about.avif"
             />
           </div>
         </div>
