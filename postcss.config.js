90c33957cddfe0626bf216899f6aeb593391c8cf
Zikrul Khalis <khaliszikrul16@gmail.com>
2025-06-18 06:45:19 +0700

    update tailwind

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js (VERSI BARU/BENAR)
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // <--- Ganti 'tailwindcss' dengan '@tailwindcss/postcss'
+    autoprefixer: {},
+  },
+};
